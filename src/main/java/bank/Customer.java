2022b8bc7d2e87a5f5ae1a59bfdf7fa88c60be8a
Divyanshu Kushwaha <78562528+divyanshu-kushwaha@users.noreply.github.com>
2026-04-05 19:47:33 +0000

    Pending changes exported from your codespace

---
+++ src/main/java/bank/Customer.java
@@ -0,0 +1,59 @@
+package bank;
+
+public class Customer {
+  private int id;
+  private String name;
+  private String username;
+  private String password;
+  private String accoundId;
+
+
+  public Customer(int id, String name, String username, String password, String accoundId) {
+    this.id = id;
+    this.name = name;
+    this.username = username;
+    this.password = password;
+    this.accoundId = accoundId;
+  }
+
+  public int getId() {
+    return this.id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return this.name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getUsername() {
+    return this.username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public String getPassword() {
+    return this.password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public String getAccoundId() {
+    return this.accoundId;
+  }
+
+  public void setAccoundId(String accoundId) {
+    this.accoundId = accoundId;
+  }
+  
+}
